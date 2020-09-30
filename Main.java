4634b31f73f5c0b657208031ba16ff3be2f029c9
uvsq21805607 <linda.bedjaoui@ens.uvsq.fr>
2020-09-30 16:17:44 +0200

    commit 3

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 
+import java.util.Scanner;
+import java.awt.Color;
+import java.awt.*;
+import java.util.*;
+import java.util.ArrayList;
 
 class Main {
 
 	public static void main(String[] args){
 
-		System.out.println(" 1+1 = 2 ");
+		 System.out.println(" Voila la fraction : \n ");
+
+		 fraction f = new fraction("1", "1", "2");
+
+		 System.out.println( f.toString());
+
+
 	} 
 }
--- fraction.java
+++ fraction.java
@@ -1,1 +1,28 @@
-hdsghud
+import java.util.Scanner;
+import java.awt.Color;
+import java.awt.*;
+import java.util.*;
+import java.util.ArrayList;
+
+public class fraction{
+
+	private String op1;
+	private String op2;
+	private String res;
+
+	public fraction(String op1, String op2, String res){
+
+		this.op1 = op1;
+		this.op2 = op2;
+		this.res = res;
+	}
+
+
+	@Override
+
+	public String toString(){
+
+		return op1 + " + " + op2 + " = " + res;
+	}
+
+}
