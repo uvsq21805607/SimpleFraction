f25ac4b2ab02d17f1765a063448567c0dc15a421
uvsq21805607 <linda.bedjaoui@ens.uvsq.fr>
2020-10-01 11:55:43 +0200

    commit 5

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args){
 
-		 System.out.println(" Voila la fraction : \n ");
+		 System.out.println(" La fraction : \n ");
 
 		 fraction f = new fraction("1", "1", "2");
 
